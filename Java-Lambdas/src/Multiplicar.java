44aab37b0d12a1a297c437d0ebc4f10b712b589f
João Pedro <126629077+joaopedrogr@users.noreply.github.com>
2025-01-31 18:31:11 -0300

    Delete Java-Lambdas/src directory

--- Java-Lambdas/src/Multiplicar.java
+++
@@ -1,6 +0,0 @@
-public class Multiplicar implements Calculo {
-
-	public double executar(double a, double b) {
-		return a * b;
-	}
-}
--- Java-Lambdas/src/Somar.java
+++
@@ -1,6 +0,0 @@
-public class Somar implements Calculo {
-
-	public double executar(double a, double b) {	
-		return a + b;
-	}
-}
